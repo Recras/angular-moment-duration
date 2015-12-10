761150196cc736cde09a075f76723f06605a6e89
Tijmen Klein <tijmen@recras.nl>
2015-12-10 10:17:07 +0100

    Reindent commit

--- src/angular-moment-duration.js
+++ src/angular-moment-duration.js
@@ -5,74 +5,72 @@
  */
 angular.module('ui.moment-duration', [])
 
-.directive('momentduration', [function () {
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        priority: 1,
-        scope: {
-            type: '@momentduration',
-            maxUnit: '@maxUnit'
-        },
-        link: function (scope, element, attrs, ngModel) {
-            ngModel.$render = function () {
-                if (!ngModel.$modelValue) {
-                    return;
-                }
-                var duration = moment.duration(ngModel.$modelValue);
-
-                var value;
-                if (attrs.maxUnit === undefined) {
-                    value = duration.get(scope.type);
-                }
-                else {
-                    value = duration.as(scope.type);
-                }
-
-                if (attrs.type === undefined) {
-                    attrs.$set('type', 'number');
-                }
-
-                if (attrs.min === undefined) {
-                    attrs.$set('min', '0');
-                }
-
-                if (attrs.max === undefined) {
-                    var maxVal;
-                    switch (scope.type) {
-                        case 'seconds':
-                        case 'minutes':
-                            maxVal = 59;
-                            break;
-                        case 'hours':
-                            maxVal = 23;
-                            break;
-                        case 'days':
-                            maxVal = 29; // 30 days == 1 month in moment
-                            break;
-                        case 'months':
-                            maxVal = 11;
-                            break;
+    .directive('momentduration', [function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            priority: 1,
+            scope: {
+                type: '@momentduration',
+                maxUnit: '@maxUnit'
+            },
+            link: function(scope, element, attrs, ngModel) {
+                ngModel.$render = function() {
+                    if (!ngModel.$modelValue) {
+                        return;
                     }
-                    if (maxVal) {
-                        attrs.$set('max', maxVal);
+                    var duration = moment.duration(ngModel.$modelValue);
+
+                    var value;
+                    if (attrs.maxUnit === undefined) {
+                        value = duration.get(scope.type);
+                    } else {
+                        value = duration.as(scope.type);
                     }
-                }
 
-                element.val(Math.floor(value));
-            };
+                    if (attrs.type === undefined) {
+                        attrs.$set('type', 'number');
+                    }
 
-            ngModel.$parsers.unshift(function (viewValue) {
-                var duration = ngModel.$modelValue;
-                var newValue;
-                if (scope.maxUnit === undefined) {
-                    newValue = duration.get(scope.type);
-                }
-                else {
-                    newValue = Math.floor(duration.as(scope.type));
-                }
-                return duration.add(moment.duration(viewValue - newValue, scope.type));
-            });
-        }
-    };
-}]);
+                    if (attrs.min === undefined) {
+                        attrs.$set('min', '0');
+                    }
+
+                    if (attrs.max === undefined) {
+                        var maxVal;
+                        switch (scope.type) {
+                            case 'seconds':
+                            case 'minutes':
+                                maxVal = 59;
+                                break;
+                            case 'hours':
+                                maxVal = 23;
+                                break;
+                            case 'days':
+                                maxVal = 29; // 30 days == 1 month in moment
+                                break;
+                            case 'months':
+                                maxVal = 11;
+                                break;
+                        }
+                        if (maxVal) {
+                            attrs.$set('max', maxVal);
+                        }
+                    }
+
+                    element.val(Math.floor(value));
+                };
+
+                ngModel.$parsers.unshift(function(viewValue) {
+                    var duration = ngModel.$modelValue;
+                    var newValue;
+                    if (scope.maxUnit === undefined) {
+                        newValue = duration.get(scope.type);
+                    } else {
+                        newValue = Math.floor(duration.as(scope.type));
+                    }
+                    return duration.add(moment.duration(viewValue - newValue, scope.type));
+                });
+            }
+        };
+    }]);
